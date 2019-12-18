7b220c179bd4627f5efc65f3e80354c20fdeff8f
Mikael Aurell <aurell.mikael@gmail.com>
2019-12-18 14:52:51 +0100

    Assignment 1

---
+++ MVC_Assignment/Controllers/FeversController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Assignment.Models;
+
+namespace MVC_Assignment.Controllers
+{
+    public class FeversController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.FeverList = Fever.feversList;
+
+            return View();
+        }
+        [HttpGet] //skapar form
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] //postar 
+        public IActionResult Create(string info)
+        {
+            if (string.IsNullOrEmpty(info))
+            {
+                ViewBag.Msg = "You must enter some text.";
+                return View();
+            }
+            else
+            {
+                Fever.feversList.Add(new Fever() { Info = info });
+
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+}
---
+++ MVC_Assignment/Models/Fever.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Assignment.Models
+{
+    public class Fever
+    {
+        static int idCounter = 0;
+        public static List<Fever> feversList = new List<Fever>();
+        public int Id { get; set; }
+        public string Info { get; set; }
+
+        public Fever()
+        {
+            Id = ++idCounter;
+        }
+    }
+}
